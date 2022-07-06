17b7d390deca85fecfbb28f1b9c741d189ce311f
putrinabila0610 <sulthanaputrinabila06@gmail.com>
2022-07-06 11:10:41 +0700

    pembuatan database

--- database/migrations/2022_07_06_035148_create_pengirimans_table.php
+++ database/migrations/2022_07_06_035148_create_pengirimans_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pengirimans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_ases',['admin','user']);
-            $table->rememberToken();
+            $table->string('kode')->unique();
+            $table->string('jenis');
+            $table->string('jumlah');
+            $table->string('harga');
+            $table->date('tgl_kirim');
+
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pengirimans');
     }
 };
--- database/migrations/2022_07_06_035243_create_updatedatabarangs_table.php
+++ database/migrations/2022_07_06_035243_create_updatedatabarangs_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('updatedatabarangs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_ases',['admin','user']);
-            $table->rememberToken();
+            $table->string('kode')->unique();
+            $table->bigInteger('jeniss_id');
+            $table->string('cabang');
+            $table->string('jumlah');
+            $table->string('harga');
+
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('updatedatabarangs');
     }
 };
